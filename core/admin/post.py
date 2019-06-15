68df475aaf76340f5a4a65494c0b3c04224c1a87
hygsani <blek.wind@gmail.com>
2019-06-16 01:38:44 +0700

    add edit form

--- core/admin/post.py
+++ core/admin/post.py
@@ -23,7 +23,14 @@ def add(request):
 
 @view_config(route_name='admin.post.edit', renderer=c_view_paths['admin'] + 'posts/edit.jinja2')
 def edit(request):
-    return dict(status='ok', view_path=c_view_paths['admin'])
+    session = Session()
+    post_id = request.matchdict['id']
+    post = session.query(Post).get(post_id)
+
+    if post == None:
+        return HTTPFound(location='/admin/post')
+
+    return dict(status='ok', view_path=c_view_paths['admin'], post=post)
 
 
 @view_config(route_name='admin.post.delete', renderer=c_view_paths['admin'] + 'post/delete.jinja2')
@@ -45,6 +52,27 @@ def do_add(request):
         created_at=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
         created_by=1
     )
+
+    session.add(post)
+    session.commit()
+
+    return HTTPFound(location='/admin/post')
+
+
+@view_config(route_name='admin.post.do_edit')
+def do_edit(request):
+    session = Session()
+    post_id = request.matchdict['id']
+    title = request.params.get('title')
+    content = request.params.get('content')
+    is_published = request.params.get('is_published')
+    post = session.query(Post).get(post_id)
+
+    post.title = title
+    post.content = content
+    post.is_published = is_published
+    post.updated_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    post.updated_by = 1
 
     session.add(post)
     session.commit()
